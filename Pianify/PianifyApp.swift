041ef6b3e447bdee324c808023e5e7aedeef8d20
GitHubSIyer <GitHubSIyer>
2025-01-27 07:27:27 -0600

    Initial Commit

---
+++ Pianify/PianifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianifyApp.swift
+//  Pianify
+//
+//  Created by Sanjana Arunkumar Iyer on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PianifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PianifyTests/PianifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PianifyTests.swift
+//  PianifyTests
+//
+//  Created by Sanjana Arunkumar Iyer on 1/27/25.
+//
+
+import Testing
+@testable import Pianify
+
+struct PianifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
