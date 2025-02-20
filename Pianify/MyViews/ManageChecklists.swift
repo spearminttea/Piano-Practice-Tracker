dc999dfdd14f560cc7dad397b5ac2003608d9abc
spearminttea <sanjanaiyer29@gmail.com>
2025-02-20 13:28:20 -0600

    update homepage vars. Create ManageChecklists

---
+++ Pianify/MyViews/ManageChecklists.swift
@@ -0,0 +1,18 @@
+//
+//  ManageChecklists.swift
+//  Pianify
+//
+//  Created by Sanjana Arunkumar Iyer on 2/20/25.
+//
+
+import SwiftUI
+
+struct ManageChecklists: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManageChecklists()
+}
