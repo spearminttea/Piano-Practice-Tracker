d261fc230352a87919e4d8c87c01f815bda66b3a
spearminttea <sanjanaiyer29@gmail.com>
2025-02-26 14:12:42 -0600

    Audio recording settings

--- Pianify/MyViews/Audio.swift
+++ Pianify/MyViews/Audio.swift
@@ -6,19 +6,66 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct Audio: View
 {
+    func setupRecordingSession() throws
+    {
+        let recordingSession = AVAudioSession.sharedInstance()
+        try recordingSession.setCategory(.playAndRecord, mode: .default)
+        try recordingSession.setActive(true)
+    }
+
+    var audioRecorder: AVAudioRecorder?
+
+    mutating func setupRecorer() throws
+    {
+        let recordingSettings = [AVFormatIDKey: kAudioFormatMPEG4AAC, AVSampleRateKey: 12000, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue] as [String : Any]
+        let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let audioFilename = documentPath.appendingPathComponent("recording.m4a")
+        
+        audioRecorder = try AVAudioRecorder(url: audioFilename, settings: recordingSettings)
+        audioRecorder?.prepareToRecord()
+    }
+    
+    func startRecording()
+    {
+        audioRecorder?.record()
+    }
+    
+    func stopRecording()
+    {
+        audioRecorder?.stop()
+    }
+    
     var body: some View
     {
+        
         ZStack
         {
-            Color.blue
+            Color.white
             
-            Image("Audio")
+            VStack
+            {
+                Button
+                {
+                    startRecording()
+                } label:
+                {
+                    Image("Audio")
+                }
+                Button ("Stop Recording")
+                {
+                    stopRecording()
+                }
+            }
         }
     }
 }
+
+
+
 
 #Preview {
     Audio()
